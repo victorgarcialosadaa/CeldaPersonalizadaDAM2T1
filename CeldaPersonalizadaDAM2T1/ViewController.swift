3c020d07e0e48c41ff6770253dbb00ecb6620441
Daniel Moreno Arellano <danielmorenoarellano@gmail.com>
2019-01-16 18:36:32 +0100

    Initial Commit

---
+++ CeldaPersonalizadaDAM2T1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CeldaPersonalizadaDAM2T1
+//
+//  Created by DAM on 16/01/2019.
+//  Copyright © 2019 DAM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ CeldaPersonalizadaDAM2T1Tests/CeldaPersonalizadaDAM2T1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  CeldaPersonalizadaDAM2T1Tests.swift
+//  CeldaPersonalizadaDAM2T1Tests
+//
+//  Created by DAM on 16/01/2019.
+//  Copyright © 2019 DAM. All rights reserved.
+//
+
+import XCTest
+@testable import CeldaPersonalizadaDAM2T1
+
+class CeldaPersonalizadaDAM2T1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
